4fe5cf85393af3a82b1f8722bc197819d44d99ab
Treisa7 <frederik.treis@gmx.de>
2021-02-09 09:02:40 +0100

    LST To Do

---
+++ app/src/main/java/de/example/lst/To_Do_List.java
@@ -0,0 +1,14 @@
+package de.example.lst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class To_Do_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to__do__list);
+    }
+}
